ef4a9b6ca94bb942cf170024540874a8515dfabf
saravanan <saravanbarani029@gmail.com>
2024-12-04 12:58:04 +0530

    first day

---
+++ index.js
@@ -0,0 +1,24 @@
+//  var amount = prompt("enter your amount: "),
+// discount = prompt("enter your discount: ");
+
+// let res = amount*discouunt/100;
+
+// console.log(amount-res);
+
+
+console.log(document
+    .getElementsByClassName("sk"));
+
+    // document.getElementsByClassName("sk")[0].style="color:red";
+    // document.getElementsByClassName("sk")[1].style="color:blue";
+    // document.getElementsByClassName("sk")[2].style="color:green";
+    // document.getElementsByClassName("sk")[3].style="color:yellow";
+    // document.getElementsByClassName("sk")[4].style="color:gray";
+    // document.getElementsByClassName("sk")[5].style="color:pink";
+
+    // for(let i=0; i<array.length;i++) {
+        // arr[i].style="color:red"
+    // }/
+    console.log(document.getElementsByClassName("h1"));
+     
+    
